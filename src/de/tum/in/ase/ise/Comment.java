helenjim95/Mock-Object-Pattern-2

Exercise-Template pushed by Artemis

src/de/tum/in/ase/eist/Comment.java
@@ -1,0 +1,16 @@
+package de.tum.in.ase.eist;
+
+public interface Comment {
+
+    boolean save();
+
+    String getText();
+
+    void setText(String text);
+
+    Person getAuthor();
+
+    void setAuthor(Person author);
+
+}
+

src/de/tum/in/ase/eist/Course.java
@@ -1,0 +1,31 @@
+package de.tum.in.ase.eist;
+
+import java.time.LocalDate;
+import java.util.List;
+
+public interface Course {
+
+    boolean isDiscussionAllowed(Person person);
+
+    String getTitle();
+
+    void setTitle(String title);
+
+    Lecturer getLecturer();
+
+    void setLecturer(Lecturer lecturer);
+
+    List<LocalDate> getDates();
+
+    void setDates(List<LocalDate> dates);
+
+    List<Student> getAttendees();
+
+    void setAttendees(List<Student> attendees);
+
+    void addAttendee(Student student);
+
+    int getNumberOfAttendees();
+
+}
+

src/de/tum/in/ase/eist/Discussion.java
@@ -1,0 +1,56 @@
+package de.tum.in.ase.eist;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Discussion {
+
+    private String topic;
+    private Course course;
+    private final List<Comment> comments = new ArrayList<>();
+
+    public boolean startCourseDiscussion(Course currentCourse, Person person, String currentTopic) {
+        if (!currentCourse.isDiscussionAllowed(person)) {
+            return false;
+        }
+        this.course = currentCourse;
+        this.topic = currentTopic;
+
+        return true;
+    }
+
+    public boolean addComment(Comment comment) {
+        if (comment.save()) {
+            comments.add(comment);
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    public String getTopic() {
+        return topic;
+    }
+
+    public void setTopic(String topic) {
+        this.topic = topic;
+    }
+
+    public Course getCourse() {
+        return course;
+    }
+
+    public void setCourse(Course course) {
+        this.course = course;
+    }
+
+    public List<Comment> getComments() {
+        return comments;
+    }
+
+    public int getNumberOfComments() {
+        return comments.size();
+    }
+
+}
+

src/de/tum/in/ase/eist/Lecturer.java
@@ -1,0 +1,37 @@
+package de.tum.in.ase.eist;
+
+import java.time.LocalDate;
+import java.util.List;
+
+public class Lecturer extends Person {
+
+    private List<Course> courses;
+    private int employeeId;
+
+    public Lecturer(String firstName, String lastName, LocalDate birthDate) {
+        super(firstName, lastName, birthDate);
+    }
+
+    public List<Course> getCourses() {
+        return courses;
+    }
+
+    public void setCourses(List<Course> courses) {
+        this.courses = courses;
+    }
+
+    public int getEmployeeId() {
+        return employeeId;
+    }
+
+    public void setEmployeeId(int employeeId) {
+        this.employeeId = employeeId;
+    }
+
+    @Override
+    public void printPersonalInformation() {
+        System.out.printf("Lecturer: %s %s, %s, %d", getFirstName(), getFirstName(), getBirthDate(), employeeId);
+    }
+
+}
+

src/de/tum/in/ase/eist/Person.java
@@ -1,0 +1,44 @@
+package de.tum.in.ase.eist;
+
+import java.time.LocalDate;
+
+public abstract class Person {
+
+    private String firstName;
+    private String lastName;
+    private LocalDate birthDate;
+
+    public Person(String firstName, String lastName, LocalDate birthDate) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.birthDate = birthDate;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public LocalDate getBirthDate() {
+        return birthDate;
+    }
+
+    public void setBirthDate(LocalDate birthDate) {
+        this.birthDate = birthDate;
+    }
+
+    public abstract void printPersonalInformation();
+
+}
+

src/de/tum/in/ase/eist/Student.java
@@ -1,0 +1,57 @@
+package de.tum.in.ase.eist;
+
+import java.time.LocalDate;
+import java.util.List;
+
+public class Student extends Person {
+
+    private String majorSubject;
+    private String minorSubject;
+    private List<Course> courses;
+    private int studentId;
+
+    public Student(String firstName, String lastName, LocalDate birthDate, String majorSubject, String minorSubject) {
+        super(firstName, lastName, birthDate);
+        this.majorSubject = majorSubject;
+        this.minorSubject = minorSubject;
+    }
+
+    public String getMajorSubject() {
+        return majorSubject;
+    }
+
+    public void setMajorSubject(String majorSubject) {
+        this.majorSubject = majorSubject;
+    }
+
+    public String getMinorSubject() {
+        return minorSubject;
+    }
+
+    public void setMinorSubject(String minorSubject) {
+        this.minorSubject = minorSubject;
+    }
+
+    public List<Course> getCourses() {
+        return courses;
+    }
+
+    public void setCourses(List<Course> courses) {
+        this.courses = courses;
+    }
+
+    public int getStudentId() {
+        return studentId;
+    }
+
+    public void setStudentId(int studentId) {
+        this.studentId = studentId;
+    }
+
+    @Override
+    public void printPersonalInformation() {
+        System.out.printf("Student: %s %s, %s, %d", getFirstName(), getLastName(), getBirthDate(), studentId);
+    }
+
+}
+

Ebrahim-Udaipurwala-1005/Mock-Object-Pattern

src/de/tum/in/ase/ise/Comment.java
@@ -1,0 +1,16 @@
+package de.tum.in.ase.ise;
+
+public interface Comment {
+
+    boolean save();
+
+    String getText();
+
+    void setText(String text);
+
+    Person getAuthor();
+
+    void setAuthor(Person author);
+
+}
+

src/de/tum/in/ase/ise/Course.java
@@ -1,0 +1,31 @@
+package de.tum.in.ase.ise;
+
+import java.time.LocalDate;
+import java.util.List;
+
+public interface Course {
+
+    boolean isDiscussionAllowed(Person person);
+
+    String getTitle();
+
+    void setTitle(String title);
+
+    Lecturer getLecturer();
+
+    void setLecturer(Lecturer lecturer);
+
+    List<LocalDate> getDates();
+
+    void setDates(List<LocalDate> dates);
+
+    List<Student> getAttendees();
+
+    void setAttendees(List<Student> attendees);
+
+    void addAttendee(Student student);
+
+    int getNumberOfAttendees();
+
+}
+

src/de/tum/in/ase/ise/Discussion.java
@@ -1,0 +1,56 @@
+package de.tum.in.ase.ise;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Discussion {
+
+    private String topic;
+    private Course course;
+    private final List<Comment> comments = new ArrayList<>();
+
+    public boolean startCourseDiscussion(Course currentCourse, Person person, String currentTopic) {
+        if (!currentCourse.isDiscussionAllowed(person)) {
+            return false;
+        }
+        this.course = currentCourse;
+        this.topic = currentTopic;
+
+        return true;
+    }
+
+    public boolean addComment(Comment comment) {
+        if (comment.save()) {
+            comments.add(comment);
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    public String getTopic() {
+        return topic;
+    }
+
+    public void setTopic(String topic) {
+        this.topic = topic;
+    }
+
+    public Course getCourse() {
+        return course;
+    }
+
+    public void setCourse(Course course) {
+        this.course = course;
+    }
+
+    public List<Comment> getComments() {
+        return comments;
+    }
+
+    public int getNumberOfComments() {
+        return comments.size();
+    }
+
+}
+

src/de/tum/in/ase/ise/Lecturer.java
@@ -1,0 +1,36 @@
+package de.tum.in.ase.ise;
+
+import java.time.LocalDate;
+import java.util.List;
+
+public class Lecturer extends Person {
+
+    private List<Course> courses;
+    private int employeeId;
+
+    public Lecturer(String firstName, String lastName, LocalDate birthDate) {
+        super(firstName, lastName, birthDate);
+    }
+
+    public List<Course> getCourses() {
+        return courses;
+    }
+
+    public void setCourses(List<Course> courses) {
+        this.courses = courses;
+    }
+
+    public int getEmployeeId() {
+        return employeeId;
+    }
+
+    public void setEmployeeId(int employeeId) {
+        this.employeeId = employeeId;
+    }
+
+    @Override
+    public void printPersonalInformation() {
+        System.out.printf("Lecturer: %s %s, %s, %d", getFirstName(), getFirstName(), getBirthDate(), employeeId);
+    }
+
+}

src/de/tum/in/ase/ise/Person.java
@@ -1,0 +1,44 @@
+package de.tum.in.ase.ise;
+
+import java.time.LocalDate;
+
+public abstract class Person {
+
+    private String firstName;
+    private String lastName;
+    private LocalDate birthDate;
+
+    public Person(String firstName, String lastName, LocalDate birthDate) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.birthDate = birthDate;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public LocalDate getBirthDate() {
+        return birthDate;
+    }
+
+    public void setBirthDate(LocalDate birthDate) {
+        this.birthDate = birthDate;
+    }
+
+    public abstract void printPersonalInformation();
+
+}
+

src/de/tum/in/ase/ise/Student.java
@@ -1,0 +1,57 @@
+package de.tum.in.ase.ise;
+
+import java.time.LocalDate;
+import java.util.List;
+
+public class Student extends Person {
+
+    private String majorSubject;
+    private String minorSubject;
+    private List<Course> courses;
+    private int studentId;
+
+    public Student(String firstName, String lastName, LocalDate birthDate, String majorSubject, String minorSubject) {
+        super(firstName, lastName, birthDate);
+        this.majorSubject = majorSubject;
+        this.minorSubject = minorSubject;
+    }
+
+    public String getMajorSubject() {
+        return majorSubject;
+    }
+
+    public void setMajorSubject(String majorSubject) {
+        this.majorSubject = majorSubject;
+    }
+
+    public String getMinorSubject() {
+        return minorSubject;
+    }
+
+    public void setMinorSubject(String minorSubject) {
+        this.minorSubject = minorSubject;
+    }
+
+    public List<Course> getCourses() {
+        return courses;
+    }
+
+    public void setCourses(List<Course> courses) {
+        this.courses = courses;
+    }
+
+    public int getStudentId() {
+        return studentId;
+    }
+
+    public void setStudentId(int studentId) {
+        this.studentId = studentId;
+    }
+
+    @Override
+    public void printPersonalInformation() {
+        System.out.printf("Student: %s %s, %s, %d", getFirstName(), getLastName(), getBirthDate(), studentId);
+    }
+
+}
+